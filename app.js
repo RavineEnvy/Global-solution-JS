7f83e6b08cdb43b7670af721aed3f228c833d773
Guilherme Freire pires <guiga_freire06@hotmail.com>
2026-06-08 21:32:54 -0300

    Adicionando Consts dos botões e formulários

---
+++ app.js
@@ -0,0 +1,13 @@
+let listaCadastrados = [];
+let listaSenhas = [];
+
+const botaoLogin = document.getElementById("btn-login");
+const botaoCadastro = document.getElementById("btn-cadastro");
+const botaoConfirmar = document.getElementById("btn-confirmar");
+const botaoConfirmarCadastro = document.getElementById("btn-confirmar-Cadastro");
+const formularioLogin = document.getElementById("form-login");
+const formularioCadastro = document.getElementById("form-cadastro");
+const botaoSlidePrev = document.getElementById("btn-slide-prev");
+const botaoSlideNext = document.getElementById("btn-slide-next");
+const slides = document.querySelectorAll(".slides img");
+const quiz = document.getElementById("quiz");
