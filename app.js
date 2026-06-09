0018bc1dfb97bf27b20466a57ff3de6dfe7f3881
Guilherme Freire pires <guiga_freire06@hotmail.com>
2026-06-08 21:34:19 -0300

    Adicionando Functions SlideShow

--- app.js
+++ app.js
@@ -11,3 +11,38 @@ const botaoSlidePrev = document.getElementById("btn-slide-prev");
 const botaoSlideNext = document.getElementById("btn-slide-next");
 const slides = document.querySelectorAll(".slides img");
 const quiz = document.getElementById("quiz");
+
+let slideIndex = 0;
+let intervaloID = null;
+
+function initializeSlider(){
+
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervaloID = setInterval(nextSlide, 9000);
+    }
+}
+
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
